f0c80fd7a1170f6e330187bc52125bb684c72cdb
lakshminellore <lakshminellore16@gmail.com>
2024-10-08 12:58:41 +0530

    Add files via upload

---
+++ calculator project.py
@@ -0,0 +1,28 @@
+#if statements
+#mini project calculator
+
+first=input("enter 1st number:")
+second=input("enter 2nd number:")
+operator=input("enter an operator(+ - * % /):")
+
+first =int(first)
+second =int(second)
+
+if operator =="+":
+     print(first +second)
+elif operator =="-":
+     print(first- second)
+elif operator =="*":
+     print(first- second)
+elif operator =="*":
+     print(first- second)
+elif operator =="%":
+     print(first % second)
+elif operator =="/":
+     print(first/ second)
+else:
+    print("please enter valid input")
+ 
+
+
+
